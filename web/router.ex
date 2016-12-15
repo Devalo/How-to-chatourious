8518853354bb0635346714929e359c447462c5c7
Stephan Valois <stephanvalois@gmail.com>
2016-12-15 20:12:32 +0100

    init + coherence

---
+++ web/router.ex
@@ -0,0 +1,49 @@
+defmodule Chatourius.Router do
+  use Chatourius.Web, :router
+  use Coherence.Router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+    plug Coherence.Authentication.Session  #  Add this
+  end
+
+  # Add this block
+  pipeline :protected do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+    plug Coherence.Authentication.Session, protected: true  # Add this
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/" do
+    pipe_through :browser
+    coherence_routes
+  end
+
+  # Add this block
+  scope "/" do
+    pipe_through :protected
+    coherence_routes :protected
+  end
+
+  scope "/", Chatourius do
+    pipe_through :browser # Use the default browser stack
+    get "/", PageController, :index
+  end
+
+  # Add this block
+  scope "/", Chatourius do
+    pipe_through :protected
+    # Add protected routes below
+  end
+end
